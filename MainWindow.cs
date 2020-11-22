14777355fd20313623d8f299aae27d8eac8e3ce1
Maksym Karapud <maks.karapud52@gmail.com>
2020-11-22 17:40:55 +0200

    First commit

---
+++ MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        combobox1.AppendText("sex");
+        combobox1.AppendText("gay");
+        combobox1.Popup();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
